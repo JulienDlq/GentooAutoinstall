1e1dff71267c3962bd207f38c11fffacb2d16b46
JulienDlq <julien.dlq@gmail.com>
2019-04-25 14:00:24 +0200

    GetCD : Script de récupération du cd d'installation minimale
    Ajout : Nouveau script pour récupérer et vérifier le cd d'installation minimale d'une Gentoo

---
+++ gentoo-get-cd.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+. ./Core/init
+
+echo "Téléchargement de l'image iso…"
+MIRROR=distfiles.gentoo.org
+IMAGE="install-amd64-minimal-\w*.iso"
+TMP=/tmp
+FILE=$(wget -q http://${MIRROR}/releases/amd64/autobuilds/latest-iso.txt -O - | grep -o -e "${IMAGE}")
+wget -c http://${MIRROR}/releases/amd64/autobuilds/current-install-amd64-minimal/$FILE -O ${TMP}/$FILE
+wget -c http://${MIRROR}/releases/amd64/autobuilds/current-install-amd64-minimal/${FILE}.DIGESTS -O ${TMP}/${FILE}.DIGESTS
+
+echo "Vérification de l'image iso…"
+cd ${TMP}
+try grep "$( sha512sum $FILE )" ${FILE}.DIGESTS
+
+exit 0
+
