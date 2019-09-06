bb07a71b192417e13d999253c0ec2e465b5a9438
JulienDlq <julien.dlq@gmail.com>
2019-09-06 17:03:46 +0200

    GetCD : Script de récupération du cd d'installation minimale
    Correction : utilisation d'IPv4

--- gentoo-get-cd.sh
+++ gentoo-get-cd.sh
@@ -6,9 +6,9 @@ echo "Téléchargement de l'image iso…"
 MIRROR=distfiles.gentoo.org
 IMAGE="install-amd64-minimal-\w*.iso"
 TMP=/tmp
-FILE=$(wget -q http://${MIRROR}/releases/amd64/autobuilds/latest-iso.txt -O - | grep -o -e "${IMAGE}")
-wget -c http://${MIRROR}/releases/amd64/autobuilds/current-install-amd64-minimal/$FILE -O ${TMP}/$FILE
-wget -c http://${MIRROR}/releases/amd64/autobuilds/current-install-amd64-minimal/${FILE}.DIGESTS -O ${TMP}/${FILE}.DIGESTS
+FILE=$(wget -4 -q http://${MIRROR}/releases/amd64/autobuilds/latest-iso.txt -O - | grep -o -e "${IMAGE}")
+wget -4 -c http://${MIRROR}/releases/amd64/autobuilds/current-install-amd64-minimal/$FILE -O ${TMP}/$FILE
+wget -4 -c http://${MIRROR}/releases/amd64/autobuilds/current-install-amd64-minimal/${FILE}.DIGESTS -O ${TMP}/${FILE}.DIGESTS
 
 echo "Vérification de l'image iso…"
 cd ${TMP}
